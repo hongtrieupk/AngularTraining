234ab6db2b3c80fac2fc5d33faa20f7cf41dfbe9
Million Nguyen <trieulhgnuyen@kms-technology.com>
2022-03-22 11:20:42 +0700

    re-adding create production function

--- helloworld/src/app/components/product-create/product-create.component.ts
+++ helloworld/src/app/components/product-create/product-create.component.ts
@@ -3,9 +3,8 @@ import { ToastrService } from 'ngx-toastr';
 
 import { ProductsService } from '../../services/products.service';
 import { ProductOverview } from '../../models/product-overview.model';
-import { NavigationService } from '../../services/navigation.service';
-import { RoutePathEnum } from 'src/app/routes.enum';
 import { NgForm } from '@angular/forms';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -19,28 +18,28 @@ export class ProductCreateComponent {
   @ViewChild('createProductForm')
   createProductForm: NgForm | undefined;
 
-  constructor() { }
+  constructor(private productService: ProductsService,
+    private toastr: ToastrService,
+    private router: Router) { }
 
   onSubmit(): void {
-    console.log(this.createProductForm?.value);
-    // or using 2-way binding
+    // TODO: apply reactive / template driven form
+    // apply validation before submitting
     console.log(this.newProduct);
+    this.newProduct.id = Math.random().toString();
+    this.productService.addProduct(this.newProduct);
+    this.toastr.success(`${this.newProduct.name} has been successfully added`);
+    this.router.navigateByUrl('products');
   }
+
+  // constructor() { }
+
+  // onSubmit(): void {
+  //   console.log(this.createProductForm?.value);
+  //   // or using 2-way binding
+  //   console.log(this.newProduct);
+  // }
 }
 
 
-// newProduct: ProductOverview = new ProductOverview('product 1', 'ttest', 0, 'img');
 
-// onSubmit(): void {
-//   // TODO: apply reactive / template driven form
-//   // apply validation before submitting
-//   console.log(this.newProduct);
-//   this.newProduct.id = Math.random().toString();
-//   this.productService.addProduct(this.newProduct);
-//   this.toastr.success(`${this.newProduct.name} has been successfully added`);
-//   // this.navigationService.setCurrentPath(RoutePathEnum.ProductList);
-// }
-
-// constructor(private productService: ProductsService,
-//   private toastr: ToastrService,
-//   private navigationService: NavigationService) { }
